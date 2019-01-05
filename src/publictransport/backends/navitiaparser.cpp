c1ed1c47006f751ab577bc70de6502323cdcb312
Volker Krause <vkrause@kde.org>
2019-01-05 13:38:50 +0100

    Fix journey time parsing

--- src/publictransport/backends/navitiaparser.cpp
+++ src/publictransport/backends/navitiaparser.cpp
@@ -131,8 +131,8 @@ static JourneySection parseJourneySection(const QJsonObject &obj)
     section.setTo(parseWrappedLocation(obj.value(QLatin1String("to")).toObject()));
     section.setRoute(route);
     // TODO realtime data
-    section.setScheduledArrivalTime(parseDateTime(obj.value(QLatin1String("departure_date_time")), section.from().timeZone()));
-    section.setScheduledDepartureTime(parseDateTime(obj.value(QLatin1String("arrival_date_time")), section.to().timeZone()));
+    section.setScheduledDepartureTime(parseDateTime(obj.value(QLatin1String("departure_date_time")), section.from().timeZone()));
+    section.setScheduledArrivalTime(parseDateTime(obj.value(QLatin1String("arrival_date_time")), section.to().timeZone()));
 
     const auto typeStr = obj.value(QLatin1String("type")).toString();
     if (typeStr == QLatin1String("public_transport")) {
