e55dc77fc1da042bc83323cec57985e5cf9f51c8
Volker Krause <vkrause@kde.org>
2020-10-21 17:39:53 +0200

    REUSE compliance for build system files

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2018 Volker Krause <vkrause@kde.org>
+# SPDX-License-Identifier: BSD-3-Clause
+
 #! /usr/bin/env bash
 $XGETTEXT `find -name \*.cpp -o -name \*.qml -o -name \*.js` -o $podir/kde-itinerary.pot
 
