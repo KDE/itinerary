020ac2de900a9cfeb691a98c35fed0b3932308fa
Volker Krause <vkrause@kde.org>
2022-06-16 18:08:05 +0200

    Implement calendar plugin loading for non-Android

--- src/calendarextras/calendarpluginloader.cpp
+++ src/calendarextras/calendarpluginloader.cpp
@@ -9,6 +9,10 @@
 #include "androidcalendarplugin.h"
 #endif
 
+#include <QCoreApplication>
+#include <QDirIterator>
+#include <QPluginLoader>
+
 using namespace KCalendarCore;
 
 struct PluginLoader {
@@ -21,7 +25,21 @@ PluginLoader::PluginLoader()
 #ifdef Q_OS_ANDROID
     plugin.reset(new AndroidCalendarPlugin(nullptr, {}));
 #else
-    // TODO
+    QStringList searchPaths(QCoreApplication::applicationDirPath());
+    searchPaths += QCoreApplication::libraryPaths();
+
+    for (const auto &searchPath : std::as_const(searchPaths)) {
+        const QString pluginPath = searchPath + QLatin1String("/kf" QT_STRINGIFY(QT_VERSION_MAJOR) "/org.kde.kcalendarcore.calendars");
+        for (QDirIterator it(pluginPath, QDir::Files); it.hasNext() && !plugin;) {
+            it.next();
+            QPluginLoader loader(it.fileInfo().absoluteFilePath());
+            if (loader.load()) {
+                plugin.reset(qobject_cast<KCalendarCore::CalendarPlugin*>(loader.instance()));
+            } else {
+                qDebug() << loader.errorString();
+            }
+        }
+    }
 #endif
 }
 
