43fd12d1b77afd003bbb3fcb14ba11440c546941
Volker Krause <vkrause@kde.org>
2025-02-22 13:14:34 +0100

    Disable DB online import for now
    The API endpoint we use doesn't respond anymore, and we currently don't
    have an easy replacement for this.

--- autotests/onlineimporttest.py
+++ autotests/onlineimporttest.py
@@ -12,23 +12,24 @@ import os
 
 # Online import form tests
 class ItineraryTest(ItineraryTestCase):
-    def test_db(self):
-        self.triggerImportAction("Deutsche Bahn Online Ticket…")
-        searchBtn = self.driver.find_element(by=AppiumBy.NAME, value="Search…")
-        self.assertFalse(searchBtn.is_enabled())
-        nameInput = self.driver.find_element(by=AppiumBy.NAME, value="Family name")
-        nameInput.send_keys("Konqi")
-        self.assertFalse(searchBtn.is_enabled())
-        refInput = self.driver.find_element(by=AppiumBy.NAME, value="Order number or booking reference")
-        refInput.send_keys("1234567")
-        self.assertFalse(searchBtn.is_enabled())
-        refInput.send_keys("12345")
-        self.assertTrue(searchBtn.is_enabled())
-        refInput.clear()
-        self.assertFalse(searchBtn.is_enabled())
-        refInput.send_keys("ABC123")
-        self.assertTrue(searchBtn.is_enabled())
-        self.goBack()
+    # FIXME currently disabled as the DB API endpoint is no longer responding
+    # def test_db(self):
+    #     self.triggerImportAction("Deutsche Bahn Online Ticket…")
+    #     searchBtn = self.driver.find_element(by=AppiumBy.NAME, value="Search…")
+    #     self.assertFalse(searchBtn.is_enabled())
+    #     nameInput = self.driver.find_element(by=AppiumBy.NAME, value="Family name")
+    #     nameInput.send_keys("Konqi")
+    #     self.assertFalse(searchBtn.is_enabled())
+    #     refInput = self.driver.find_element(by=AppiumBy.NAME, value="Order number or booking reference")
+    #     refInput.send_keys("1234567")
+    #     self.assertFalse(searchBtn.is_enabled())
+    #     refInput.send_keys("12345")
+    #     self.assertTrue(searchBtn.is_enabled())
+    #     refInput.clear()
+    #     self.assertFalse(searchBtn.is_enabled())
+    #     refInput.send_keys("ABC123")
+    #     self.assertTrue(searchBtn.is_enabled())
+    #     self.goBack()
 
     def test_sncf(self):
         self.triggerImportAction("SNCF Online Ticket…")
