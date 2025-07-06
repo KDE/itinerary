ebb6ba4ce8a1a7437b4ffd999c11c0283d59fbd1
Volker Krause <vkrause@kde.org>
2025-07-06 12:12:18 +0200

    Add methods to create Matrix state events for uploaded documents and passes

--- src/app/matrixsynccontent.cpp
+++ src/app/matrixsynccontent.cpp
@@ -5,9 +5,11 @@
 
 #include "matrixsynccontent.h"
 
+#include "documentmanager.h"
 #include "livedatamanager.h"
 #include "logging.h"
 #include "matrixsyncstateevent.h"
+#include "pkpassmanager.h"
 #include "reservationmanager.h"
 #include "transfermanager.h"
 
@@ -113,6 +115,21 @@ void MatrixSyncContent::readTransfer(const MatrixSyncStateEvent &event, Transfer
     } else {
         transferMgr->importTransfer(transfer);
     }
+}
+
+MatrixSyncStateEvent MatrixSyncContent::stateEventForDocument(const QString &docId, const DocumentManager *docMgr)
+{
+    MatrixSyncStateEvent state(MatrixSync::DocumentEventType, docId);
+    state.setFileName(docMgr->documentFilePath(docId));
+    state.setExtraData("metadata"_L1, KItinerary::JsonLdDocument::toJson(docMgr->documentInfo(docId)));
+    return state;
+}
+
+MatrixSyncStateEvent MatrixSyncContent::stateEventForPkPass(const QString &passId)
+{
+    MatrixSyncStateEvent state(MatrixSync::DocumentEventType, passId);
+    state.setFileName(PkPassManager::passPath(passId));
+    return state;
 }
 
 #endif
