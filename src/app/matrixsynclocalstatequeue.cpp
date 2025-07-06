2460d83d0ec8d637c704729d6d4f6db6464d0d93
Volker Krause <vkrause@kde.org>
2025-07-06 18:37:42 +0200

    Add persisted change queue
    This contains all Matrix state event changes and document uploads we yet
    want to do, and is supposed to be replayed one by one as the Matrix
    connection is available.

---
+++ src/app/matrixsynclocalstatequeue.cpp
@@ -0,0 +1,183 @@
+/*
+    SPDX-FileCopyrightText: ⓒ 2024 Volker Krause <vkrause@kde.org>
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "matrixsynclocalstatequeue.h"
+
+#include "jsonio.h"
+#include "logging.h"
+
+#include <QDir>
+#include <QFile>
+#include <QJsonArray>
+#include <QJsonObject>
+#include <QMetaEnum>
+#include <QStandardPaths>
+
+using namespace Qt::Literals;
+
+QJsonObject MatrixSyncLocalStateChange::toJson(const MatrixSyncLocalStateChange &change)
+{
+    return QJsonObject{{
+        {"type"_L1, QLatin1StringView(QMetaEnum::fromType<MatrixSyncLocalStateQueue::ChangeType>().valueToKey(change.type))},
+        {"id"_L1, change.id},
+        {"context"_L1, change.context},
+    }};
+}
+
+std::optional<MatrixSyncLocalStateChange> MatrixSyncLocalStateChange::fromJson(const QJsonObject &obj)
+{
+    bool ok = false;
+    const auto type = QMetaEnum::fromType<MatrixSyncLocalStateQueue::ChangeType>().keyToValue(obj.value("type"_L1).toString().toUtf8().constData(), &ok);
+    if (!ok) {
+        return {};
+    }
+    return MatrixSyncLocalStateChange{
+        .type = static_cast<MatrixSyncLocalStateQueue::ChangeType>(type),
+        .id = obj.value("id"_L1).toString(),
+        .context = obj.value("context"_L1).toString(),
+    };
+}
+
+bool MatrixSyncLocalStateChange::operator==(const MatrixSyncLocalStateChange &other) const
+{
+    return type == other.type && id == other.id && context == other.context;
+}
+
+MatrixSyncLocalStateQueue::MatrixSyncLocalStateQueue(QObject *parent) :
+    QObject(parent)
+{
+    load();
+}
+
+MatrixSyncLocalStateQueue::~MatrixSyncLocalStateQueue() = default;
+
+void MatrixSyncLocalStateQueue::append(ChangeType type, const QString &id, const QString &context)
+{
+    if (m_suspended) {
+        return;
+    }
+
+    MatrixSyncLocalStateChange change{ .type = type, .id = id, .context = context};
+    if (m_pendingChanges.size() > 1 && std::ranges::find(m_pendingChanges, change) != m_pendingChanges.end()) {
+        return;
+    }
+
+    m_pendingChanges.push_back(std::move(change));
+    qDebug() << "queuing local state change" << type << id << m_pendingChanges.size();
+    store();
+    if (m_pendingChanges.size() == 1) {
+        doReplayNext();
+    }
+}
+
+void MatrixSyncLocalStateQueue::replayNext()
+{
+    assert(!m_pendingChanges.empty());
+    qDebug() << "completed state change" << m_pendingChanges.front().type << m_pendingChanges.front().id << m_pendingChanges.size();
+
+    m_pendingChanges.pop_front();
+    store();
+    if (!m_pendingChanges.empty()) {
+        QMetaObject::invokeMethod(this, &MatrixSyncLocalStateQueue::doReplayNext, Qt::QueuedConnection);
+    }
+}
+
+void MatrixSyncLocalStateQueue::retry()
+{
+    doReplayNext();
+}
+
+void MatrixSyncLocalStateQueue::suspend()
+{
+    assert(!m_suspended);
+    m_suspended = true;
+}
+
+bool MatrixSyncLocalStateQueue::isSuspended() const
+{
+    return m_suspended;
+}
+
+void MatrixSyncLocalStateQueue::resume()
+{
+    assert(m_suspended);
+    m_suspended = false;
+}
+
+QString MatrixSyncLocalStateQueue::path()
+{
+    return QStandardPaths::writableLocation(QStandardPaths::AppDataLocation) + "/matrixsync/localstatequeue"_L1;
+}
+
+void MatrixSyncLocalStateQueue::load()
+{
+    QFile f(path());
+    if (!f.open(QFile::ReadOnly)) {
+        return; // no pending changes
+    }
+
+    const auto array = JsonIO::read(f.readAll()).toArray();
+    for (const auto &obj : array) {
+        auto c = MatrixSyncLocalStateChange::fromJson(obj.toObject());
+        if (c) {
+            m_pendingChanges.push_back(std::move(*c));
+        }
+    }
+}
+
+void MatrixSyncLocalStateQueue::store()
+{
+    if (m_pendingChanges.empty()) {
+        QFile::remove(MatrixSyncLocalStateQueue::path());
+    } else {
+        QDir().mkpath(QStandardPaths::writableLocation(QStandardPaths::AppDataLocation) + "/matrixsync/"_L1);
+
+        QJsonArray array;
+        for (const auto &change :m_pendingChanges) {
+            array.push_back(MatrixSyncLocalStateChange::toJson(change));
+        }
+        QFile f(path());
+        if (!f.open(QFile::WriteOnly | QFile::Truncate)) {
+            qCWarning(Log) << f.errorString() << f.fileName();
+            return;
+        }
+        f.write(JsonIO::write(array));
+    }
+}
+
+void MatrixSyncLocalStateQueue::doReplayNext()
+{
+    if (m_pendingChanges.empty()) {
+        return;
+    }
+
+    const auto &c = m_pendingChanges.front();
+    qDebug() << "replaying local state" << c.type << c.id;
+    switch (c.type) {
+        case BatchChange:
+            Q_EMIT batchChanged(c.id, c.context);
+            break;
+        case BatchRemove:
+            Q_EMIT batchRemoved(c.id, c.context);
+            break;
+        case LiveDataChange:
+            Q_EMIT liveDataChanged(c.id);
+            break;
+        case TransferChange:
+        {
+            const auto [id, align] = Transfer::parseIdentifier(c.id);
+            Q_EMIT transferChanged(id, align);
+            break;
+        }
+        case DocumentAdd:
+            Q_EMIT documentAdded(c.id, c.context);
+            break;
+        case PkPassChange:
+            Q_EMIT pkPassChanged(c.id, c.context);
+            break;
+    }
+}
+
+#include "moc_matrixsynclocalstatequeue.cpp"
---
+++ src/app/matrixsynclocalstatequeue.h
@@ -0,0 +1,110 @@
+/*
+    SPDX-FileCopyrightText: ⓒ 2024 Volker Krause <vkrause@kde.org>
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#ifndef MATRIXSYNCLOCALSTATEQUEUE_H
+#define MATRIXSYNCLOCALSTATEQUEUE_H
+
+#include "transfer.h"
+
+#include <QObject>
+
+#include <deque>
+
+class QJsonObject;
+
+class MatrixSyncLocalStateChange;
+
+/** Pending local changes awaiting upload. */
+class MatrixSyncLocalStateQueue : public QObject
+{
+    Q_OBJECT
+public:
+    explicit MatrixSyncLocalStateQueue(QObject *parent = nullptr);
+    ~MatrixSyncLocalStateQueue();
+
+    enum ChangeType {
+        BatchChange,
+        BatchRemove,
+        LiveDataChange,
+        TransferChange,
+        DocumentAdd,
+        PkPassChange,
+    };
+    Q_ENUM(ChangeType)
+
+    /** Enqueue a new change.
+     *  Will be discarded when change tracking is suspended.
+     */
+    void append(ChangeType type, const QString &id, const QString &context = {});
+    /** Current change successfully applied. */
+    void replayNext();
+    /** Retry the current change, if any. */
+    void retry();
+
+    /** Suspend recording changes.
+     *  Do this while applying remote changes localls to avoid recursion.
+     *  New changes will be ignored while suspended.
+     */
+    void suspend();
+    [[nodiscard]] bool isSuspended() const;
+    /** Resume recording. */
+    void resume();
+
+    [[nodiscard]] bool isEmpty() const { return m_pendingChanges.empty(); }
+    [[nodiscard]] const MatrixSyncLocalStateChange& front() const { return m_pendingChanges.front(); }
+
+    // for testing only
+    [[nodiscard]] static QString path();
+
+Q_SIGNALS:
+    void batchChanged(const QString &batchId, const QString &tgId);
+    void batchRemoved(const QString &batchId, const QString &tgId);
+    void liveDataChanged(const QString &batchId);
+    void transferChanged(const QString &batchId, Transfer::Alignment alignment);
+    void documentAdded(const QString &docId, const QString &tgId);
+    void pkPassChanged(const QString &pkPassId, const QString &tgId);
+    // TODO trip group changes?
+
+private:
+    void store();
+    void load();
+    void doReplayNext();
+
+    std::deque<MatrixSyncLocalStateChange> m_pendingChanges;
+    bool m_suspended = false;
+};
+
+class MatrixSyncLocalStateChange {
+public:
+    [[nodiscard]] bool operator==(const MatrixSyncLocalStateChange &other) const;
+
+    [[nodiscard]] static QJsonObject toJson(const MatrixSyncLocalStateChange &change);
+    [[nodiscard]] static std::optional<MatrixSyncLocalStateChange> fromJson(const QJsonObject &obj);
+
+    MatrixSyncLocalStateQueue::ChangeType type;
+    QString id;
+    QString context;
+};
+
+/** RAII type to suspend local change recording. */
+class MatrixSyncLocalChangeLock
+{
+public:
+    explicit MatrixSyncLocalChangeLock(MatrixSyncLocalStateQueue *queue)
+        : m_queue(queue)
+    {
+        m_queue->suspend();
+    }
+
+    ~MatrixSyncLocalChangeLock()
+    {
+        m_queue->resume();
+    }
+
+private:
+    MatrixSyncLocalStateQueue *m_queue;
+};
+
+#endif
