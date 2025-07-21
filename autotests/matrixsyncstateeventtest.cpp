a5963a0402243a7c7db3154a2b23729604baa8cc
Volker Krause <vkrause@kde.org>
2025-07-21 17:07:54 +0200

    Add Matrix state event wrapper for transparently supporting larger content
    Matrix state events are limited to 65kB, we can exceed that even for other
    things than attachments. If we do, upload those as encrypted files as well.

---
+++ autotests/matrixsyncstateeventtest.cpp
@@ -0,0 +1,126 @@
+/*
+    SPDX-FileCopyrightText: ⓒ 2025 Volker Krause <vkrause@kde.org>
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "matrixsyncstateevent.h"
+
+#include <Quotient/events/stateevent.h>
+
+#include <QFile>
+#include <QStandardPaths>
+#include <QTemporaryFile>
+#include <QtTest/qtest.h>
+
+using namespace Qt::Literals;
+
+class MatrixSyncStateEventTest : public QObject
+{
+    Q_OBJECT
+private Q_SLOTS:
+    void initTestCase()
+    {
+        QStandardPaths::setTestModeEnabled(true);
+    }
+
+    void testStateEventOutbound()
+    {
+        auto ev = MatrixSyncStateEvent(MatrixSync::ReservationEventType, u"12345"_s);
+        QVERIFY(!ev.needsDownload());
+        QVERIFY(!ev.needsUpload());
+        {
+            auto state = ev.toQuotient();
+            QCOMPARE(state.matrixType(), MatrixSync::ReservationEventType);
+            QCOMPARE(state.stateKey(), "12345"_L1);
+            QCOMPARE(state.contentJson().value("version"_L1).toInt(), 1);
+            QCOMPARE(state.contentJson().value("content"_L1).toString(), QString());
+        }
+
+        ev.setContent("ABC");
+        QVERIFY(!ev.needsDownload());
+        QVERIFY(!ev.needsUpload());
+        {
+            auto state = ev.toQuotient();
+            QCOMPARE(state.contentJson().value("contentType"_L1).toString(), "none"_L1);
+            QCOMPARE(state.contentJson().value("content"_L1).toString(), "ABC"_L1);
+        }
+        QCOMPARE(ev.content(), "ABC");
+
+        ev.setContent(QByteArray(32, '\0'));
+        {
+            auto state = ev.toQuotient();
+            QCOMPARE(state.contentJson().value("contentType"_L1).toString(), "base64"_L1);
+            QCOMPARE(state.contentJson().value("content"_L1).toString(), "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="_L1);
+        }
+        QCOMPARE(ev.content(), QByteArray(32, '\0'));
+
+        ev = MatrixSyncStateEvent(MatrixSync::ReservationEventType, u"12345"_s);
+        ev.setContent(QByteArray(65523, '\0'));
+        ev.setExtraData("metaData"_L1, "BBB"_L1);
+        QVERIFY(!ev.needsDownload());
+        QVERIFY(ev.needsUpload());
+        QVERIFY(!ev.fileName().isEmpty());
+
+        Quotient::EncryptedFileMetadata md;
+        md.url = QUrl(u"mxc://foo"_s);
+        ev.setFileInfo(md);
+        QVERIFY(!ev.needsUpload());
+        {
+            auto state = ev.toQuotient();
+            QCOMPARE(state.contentJson().value("contentType"_L1).toString(), "file"_L1);
+            QCOMPARE(state.contentJson().contains("content"_L1), false);
+            QCOMPARE(state.contentJson().value("file"_L1).toObject().value("url"_L1).toString(), "mxc://foo"_L1);
+            QCOMPARE(state.contentJson().value("extra"_L1).toObject(), QJsonObject({{ "metaData"_L1, "BBB"_L1}}));
+        }
+    }
+
+    void testStateEventInbound()
+    {
+        auto ev = MatrixSyncStateEvent::fromQuotient(Quotient::StateEvent(MatrixSync::LiveDataEventType, u"12345"_s, QJsonObject{{
+            { "contentType"_L1, "none"_L1},
+            { "content"_L1, "test"_L1 },
+        }}), u"room42"_s);
+        QVERIFY(!ev);
+
+        ev = MatrixSyncStateEvent::fromQuotient(Quotient::StateEvent(MatrixSync::LiveDataEventType, u"12345"_s, QJsonObject{{
+            { "version"_L1, 1},
+            { "contentType"_L1, "none"_L1},
+            { "content"_L1, "test"_L1 },
+        }}), u"room42"_s);
+        QVERIFY(ev);
+        QVERIFY(!ev->needsDownload());
+        QVERIFY(!ev->needsUpload());
+        QCOMPARE(ev->type(), MatrixSync::LiveDataEventType);
+        QCOMPARE(ev->stateKey(), "12345"_L1);
+        QCOMPARE(ev->content(), "test");
+        QCOMPARE(ev->roomId(), "room42"_L1);
+
+        ev = MatrixSyncStateEvent::fromQuotient(Quotient::StateEvent(MatrixSync::LiveDataEventType, u"12345"_s, QJsonObject{{
+            { "version"_L1, 1},
+            { "contentType"_L1, "file"_L1},
+            { "file"_L1, QJsonObject{{
+                { "url"_L1, "mxc://foo"_L1 },
+            }}},
+            { "extra"_L1, QJsonObject{{
+                { "metaData"_L1, "ABC"_L1 }
+            }}}
+        }}), u"room42"_s);
+        QVERIFY(ev);
+        QVERIFY(ev->needsDownload());
+        QVERIFY(!ev->needsUpload());
+
+        QTemporaryFile f;
+        QVERIFY(f.open());
+        f.write("external content");
+        f.close();
+        ev->setFileName(f.fileName());
+        QVERIFY(!ev->needsDownload());
+        QVERIFY(!ev->needsUpload());
+        QCOMPARE(ev->content(), "external content");
+        QCOMPARE(ev->extraData("metaData"_L1), "ABC"_L1);
+    }
+};
+
+QTEST_GUILESS_MAIN(MatrixSyncStateEventTest)
+
+#include "matrixsyncstateeventtest.moc"
---
+++ src/app/matrixsyncstateevent.cpp
@@ -0,0 +1,214 @@
+/*
+    SPDX-FileCopyrightText: ⓒ 2025 Volker Krause <vkrause@kde.org>
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "matrixsyncstateevent.h"
+
+#if HAVE_MATRIX
+#include <Quotient/events/stateevent.h>
+#endif
+
+#include <QFile>
+#include <QSysInfo>
+#include <QTemporaryFile>
+
+using namespace Qt::Literals;
+
+constexpr inline auto FORMAT_VERSION = 1;
+constexpr inline auto CONTENT_SIZE_LIMIT = 49152;
+
+MatrixSyncStateEvent::MatrixSyncStateEvent() = default;
+
+MatrixSyncStateEvent::MatrixSyncStateEvent(QLatin1StringView type, QString stateKey)
+    : m_type(type)
+    , m_stateKey(std::move(stateKey))
+{
+    m_content.insert("version"_L1, FORMAT_VERSION);
+
+    // for easier debugging: indicate which instance wrote this state event
+    if (const auto hostName = QSysInfo::machineHostName(); !hostName.isEmpty()) {
+        m_content.insert("source"_L1, hostName);
+    }
+}
+
+MatrixSyncStateEvent::MatrixSyncStateEvent(MatrixSyncStateEvent &&) noexcept = default;
+MatrixSyncStateEvent::~MatrixSyncStateEvent() = default;
+MatrixSyncStateEvent& MatrixSyncStateEvent::operator=(MatrixSyncStateEvent &&) noexcept = default;
+
+QString MatrixSyncStateEvent::type() const
+{
+    return m_type;
+}
+
+QString MatrixSyncStateEvent::stateKey() const
+{
+    return m_stateKey;
+}
+
+#if HAVE_MATRIX
+bool MatrixSyncStateEvent::needsDownload() const
+{
+    return !url().isEmpty() && m_fileName.isEmpty();
+}
+
+bool MatrixSyncStateEvent::needsUpload() const
+{
+    return url().isEmpty() && !fileName().isEmpty();
+}
+
+QUrl MatrixSyncStateEvent::url() const
+{
+    return Quotient::getUrlFromSourceInfo(encryptionData());
+}
+
+Quotient::EncryptedFileMetadata MatrixSyncStateEvent::encryptionData() const
+{
+    Quotient::EncryptedFileMetadata encryptionData;
+    Quotient::JsonObjectConverter<Quotient::EncryptedFileMetadata>::fillFrom(m_content.value("file"_L1).toObject(), encryptionData);
+    return encryptionData;
+}
+
+void MatrixSyncStateEvent::setFileInfo(const Quotient::FileSourceInfo &info)
+{
+    QJsonObject file;
+    Quotient::JsonObjectConverter<Quotient::EncryptedFileMetadata>::dumpTo(file, std::get<Quotient::EncryptedFileMetadata>(info));
+    m_content.insert("file"_L1, file);
+}
+#endif
+
+QString MatrixSyncStateEvent::fileName() const
+{
+    return m_tempFile ? m_tempFile->fileName() : m_fileName;
+}
+
+void MatrixSyncStateEvent::setFileName(const QString &fileName)
+{
+    // TODO we could also store small enough files inline here
+    m_fileName = fileName;
+}
+
+QByteArray MatrixSyncStateEvent::content() const
+{
+    switch (contentType()) {
+        case None:
+            return m_content.value("content"_L1).toString().toUtf8();
+        case Base64:
+            return QByteArray::fromBase64(m_content.value("content"_L1).toString().toUtf8());
+        case File:
+        {
+            QFile f(m_fileName);
+            if (!f.open(QFile::ReadOnly)) {
+                qDebug() << f.errorString() << f.fileName();
+            }
+            return f.readAll();
+        }
+    }
+    return {};
+}
+
+void MatrixSyncStateEvent::setContent(const QByteArray &content)
+{
+    // look for characters that would need some form of quoting in JSON (and thus increase the size)
+    qsizetype quotingCost = 0;
+    if (content.size() < CONTENT_SIZE_LIMIT) {
+        for (const auto c : content) {
+            if (c == '\n' || c == '"') {
+                ++quotingCost;
+            } else if (c < 20 || c >= 127) {
+                quotingCost += 3;
+            }
+        }
+    }
+
+    const auto jsonSize = content.size() + quotingCost;
+    const auto base64Size = content.size() * 4 / 3;
+
+    if (std::min(jsonSize, base64Size) > CONTENT_SIZE_LIMIT) {
+        m_tempFile = std::make_unique<QTemporaryFile>();
+        if (!m_tempFile->open()) {
+            qWarning() << m_tempFile->errorString() << m_tempFile->fileName();
+        }
+        m_tempFile->write(content);
+        m_tempFile->close();
+        m_content.insert("contentType"_L1, "file"_L1);
+        return;
+    }
+
+    if (jsonSize < base64Size) {
+        m_content.insert("content"_L1, QString::fromUtf8(content));
+        m_content.insert("contentType"_L1, "none"_L1);
+    } else {
+        m_content.insert("content"_L1, QString::fromLatin1(content.toBase64()));
+        m_content.insert("contentType"_L1, "base64"_L1);
+    }
+}
+
+QString MatrixSyncStateEvent::roomId() const
+{
+    return m_roomId;
+}
+
+QJsonValue MatrixSyncStateEvent::extraData(QLatin1StringView key) const
+{
+    return m_content.value("extra"_L1).toObject().value(key);
+}
+
+void MatrixSyncStateEvent::setExtraData(QLatin1StringView key, const QJsonValue &value)
+{
+    auto extras = m_content.value("extra"_L1).toObject();
+    extras.insert(key, value);
+    m_content.insert("extra"_L1, extras);
+}
+
+#if HAVE_MATRIX
+Quotient::StateEvent MatrixSyncStateEvent::toQuotient() const
+{
+    // FIXME can we have a QString overload in Quotient for this?
+    return Quotient::StateEvent(QLatin1StringView(m_type.toLatin1()), m_stateKey, m_content);
+}
+
+std::optional<MatrixSyncStateEvent> MatrixSyncStateEvent::fromQuotient(const Quotient::StateEvent &state, const QString &roomId)
+{
+    bool found = false;
+    for (const auto t : {MatrixSync::ReservationEventType, MatrixSync::LiveDataEventType, MatrixSync::TransferEventType, MatrixSync::DocumentEventType, MatrixSync::PkPassEventType}) {
+        found = state.matrixType() == t;
+        if (found) {
+            break;
+        }
+    }
+    if (!found) {
+        return {};
+    }
+
+    if (const auto version = state.contentJson().value("version"_L1).toInt(std::numeric_limits<int>::max()); version >FORMAT_VERSION) {
+        qDebug() << "Got state event of unsupported version" << version;
+        return {};
+    }
+
+    MatrixSyncStateEvent ev;
+    ev.m_type = state.matrixType();
+    ev.m_stateKey = state.stateKey();
+    ev.m_content = state.contentJson();
+    ev.m_roomId = roomId; // NOTE: state.roomId() isn't good enough here, that's empty for incoming events from Quotient
+
+    return ev;
+}
+#endif
+
+MatrixSyncStateEvent::ContentType MatrixSyncStateEvent::contentType() const
+{
+    const auto ct = m_content.value("contentType"_L1).toString();
+    if (ct == "none"_L1) {
+        return None;
+    }
+    if (ct == "base64"_L1) {
+        return Base64;
+    }
+    if (ct == "file"_L1) {
+        return File;
+    }
+
+    qDebug() << "Unknown state event content type:" << ct;
+    return None;
+}
